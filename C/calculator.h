6a1249551a4cf797e0f4f4674a88baff12fec41e
Ayodele Kadiri <kadelcode@gmail.com>
2024-06-27 21:14:38 +0100

    Add header file containing function declarations

---
+++ C/calculator.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+void add(double a, double b);
+void subtract(double a, double b);
+void multiply(double a, double b);
+void divide(double a, double b);
+
+#endif
