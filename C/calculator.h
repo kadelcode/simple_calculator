afa6e5f8ae0bdd189f1c42001a433fc7b91d3938
Ayodele Kadiri <kadelcode@gmail.com>
2024-06-27 21:49:54 +0100

    Change return type to `double` in function declarations

--- C/calculator.h
+++ C/calculator.h
@@ -1,9 +1,9 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
-void add(double a, double b);
-void subtract(double a, double b);
-void multiply(double a, double b);
-void divide(double a, double b);
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
 
 #endif
